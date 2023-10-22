e873676980ccc5433a329a8162def064cd02688f
Gyeongaa <soogyeong00@gmail.com>
2023-10-22 22:15:24 +0900

    1. create korean.py : play kr time 2. create settings.py : basic functions to help playing sounds 3. update main.py (include korean sounds)

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import tkinter as tk
 from datetime import datetime
 import pytz
+
 from PIL import Image, ImageTk
 
+from korean import kr_speak_the_clock
+
+
 # Create the main window
-root = tk.Tk()
-root.title("Asian Time")
+mainUI = tk.Tk()
+mainUI.title("Asian Time")
 
 # Use a relative path to open the background image
-background_image = Image.open("Asia Map with Flag Icons.png")
+background_image = Image.open("images/background_map.png")
 background_image = background_image.resize(
-    (root.winfo_screenwidth(), root.winfo_screenheight())
+    (mainUI.winfo_screenwidth(), mainUI.winfo_screenheight())
 )
 background_photo = ImageTk.PhotoImage(background_image)
 
 # Set the window size to match the background image size
-root.geometry(f"{background_image.width}x{background_image.height}")
+mainUI.geometry(f"{background_image.width}x{background_image.height}")
 
 # Create a Label to display the background image, using relwidth and relheight to fill the entire window
-background_label = tk.Label(root, image=background_photo)
+background_label = tk.Label(mainUI, image=background_photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Create a label for the default time text
 default_time_label = tk.Label(
-    root, text="Default: Singapore time", font=("Helvetica", 24, "bold"), bg="white"
+    mainUI, text="Default: Singapore time", font=("Helvetica", 24, "bold"), bg="white"
 )
-default_time_label.place(x=20, y=root.winfo_screenheight() // 2 - 24, anchor="w")
+default_time_label.place(x=20, y=mainUI.winfo_screenheight() // 2 - 24, anchor="w")
 
 # Create a label for displaying the current time
-current_time_label = tk.Label(root, text="", font=("Helvetica", 24))
-current_time_label.place(x=20, y=root.winfo_screenheight() // 2 + 24, anchor="w")
+current_time_label = tk.Label(mainUI, text="", font=("Helvetica", 24))
+current_time_label.place(x=20, y=mainUI.winfo_screenheight() // 2 + 24, anchor="w")
 
 
 # Function to update the Singapore time label
@@ -38,7 +42,7 @@ def update_singapore_time():
     current_time = datetime.now(singapore_timezone)
     time_str = current_time.strftime("%Y-%m-%d %I:%M:%S %p")
     current_time_label.config(text=time_str)
-    root.after(1000, update_singapore_time)  # Update every second
+    mainUI.after(1000, update_singapore_time)  # Update every second
 
 
 # Start updating the Singapore time label
@@ -53,7 +57,7 @@ def show_world_time(timezone_name):
     time_str = time_str.lstrip("0").replace(" 0", " ")
 
     # Create a new window to display local time
-    local_time_window = tk.Toplevel(root)
+    local_time_window = tk.Toplevel(mainUI)
     local_time_window.title("Local Time")
     local_time_window.geometry("300x100")
     time_label = tk.Label(local_time_window, text=time_str, font=("Helvetica", 20))
@@ -62,31 +66,32 @@ def show_world_time(timezone_name):
 
 # Create buttons for different countries
 button1 = tk.Button(
-    root,
+    mainUI,
     text="CHINA",
     compound=tk.TOP,
     command=lambda: show_world_time("Asia/Shanghai"),
 )
 button2 = tk.Button(
-    root,
+    mainUI,
     text="JAPAN",
     compound=tk.TOP,
     command=lambda: show_world_time("Asia/Tokyo"),
 )
 button3 = tk.Button(
-    root,
+    mainUI,
     text="KOREA",
     compound=tk.TOP,
-    command=lambda: show_world_time("Asia/Seoul"),
+    command=lambda: kr_speak_the_clock()
+    #show_world_time("Asia/Seoul"),
 )
 button4 = tk.Button(
-    root,
+    mainUI,
     text="THAILAND",
     compound=tk.TOP,
     command=lambda: show_world_time("Asia/Bangkok"),
 )
 button5 = tk.Button(
-    root,
+    mainUI,
     text="SINGAPORE",
     compound=tk.TOP,
     command=lambda: show_world_time("Asia/Singapore"),
@@ -103,29 +108,29 @@ button5.pack(pady=10)
 # Function to update button positions based on window size
 def update_button_positions():
     button1.place(
-        x=830 * root.winfo_width() / background_image.width,
-        y=430 * root.winfo_height() / background_image.height,
+        x=830 * mainUI.winfo_width() / background_image.width,
+        y=430 * mainUI.winfo_height() / background_image.height,
     )
     button2.place(
-        x=1250 * root.winfo_width() / background_image.width,
-        y=400 * root.winfo_height() / background_image.height,
+        x=1250 * mainUI.winfo_width() / background_image.width,
+        y=400 * mainUI.winfo_height() / background_image.height,
     )
     button3.place(
-        x=1120 * root.winfo_width() / background_image.width,
-        y=380 * root.winfo_height() / background_image.height,
+        x=1120 * mainUI.winfo_width() / background_image.width,
+        y=380 * mainUI.winfo_height() / background_image.height,
     )
     button4.place(
-        x=800 * root.winfo_width() / background_image.width,
-        y=730 * root.winfo_height() / background_image.height,
+        x=800 * mainUI.winfo_width() / background_image.width,
+        y=730 * mainUI.winfo_height() / background_image.height,
     )
     button5.place(
-        x=790 * root.winfo_width() / background_image.width,
-        y=930 * root.winfo_height() / background_image.height,
+        x=790 * mainUI.winfo_width() / background_image.width,
+        y=930 * mainUI.winfo_height() / background_image.height,
     )
 
 
 # Bind a function to the <Configure> event to update button positions
-root.bind("<Configure>", lambda event: update_button_positions())
+mainUI.bind("<Configure>", lambda event: update_button_positions())
 
 # Start the main Tkinter event loop
-root.mainloop()
+mainUI.mainloop()
---
+++ settings.py
@@ -0,0 +1,107 @@
+"""
+Contains functions common to all language logic files.
+Also contains vital code for audio input, concatenation, and output.
+
+Authors: Dragos Balan & Hubert Matuszewski
+"""
+import librosa
+import numpy as np
+import pygame
+import soundfile as sf
+from scipy.io import wavfile
+import time
+
+
+# Function used to get the time that we will process to get the correct
+# audio files
+def get_current_time():
+    current_time = time.strftime('%H:%M')
+    split_time = current_time.split(':')
+    return int(split_time[0]), int(split_time[1])
+
+
+# Function used to cut the silence at the beginning and end of an audio signal
+def cut_silence(signal,sr=16000,threshold=0.09,padding=40):
+    # Max amplitude
+    max_signal = max(signal)
+    # Determine the threshold as %age of max amplitude
+    _threshold = threshold * max_signal
+    # Based on the sampling rate, we determine the length of the padding
+    # (converted to seconds)
+    len_padding = (padding / 1000) * sr
+    # Indices of our beginning and end stage.
+    cutoff = np.where(signal > _threshold)
+    # Extract the actual array
+    _cutoff = cutoff[0]
+    # Take the first element and go back by the amount of samples stipulated
+    # in the padding
+    first_cutoff = int(_cutoff[0] - len_padding)
+    # Take the last element and go forwards by the amount of samples stipulated
+    # in the padding
+    last_cutoff = int(_cutoff[-1] + len_padding)
+    if first_cutoff < 0:
+        # In case someone passes a padding which is too large,
+        # to avoid negative indices
+        first_cutoff = 0
+    if last_cutoff > len(signal):
+        # In case someone passes a padding which is too large,
+        # to avoid going out of range of array
+        last_cutoff = len(signal)
+    # Trim our signal according to our calculated endpoints
+    _signal = signal[first_cutoff:last_cutoff]
+    signal = _signal
+    return signal
+
+
+# Function used for reading the audio
+def read_audio(filename: str, path: str):
+    audio, sr = librosa.load(path + filename, sr=None)
+    #sr, audio = wavfile.read(path + filename)
+    # Audio in Romanian has already been manually processed, thus no extra
+    # processing is required
+    if path != './RomanianAudio/':
+        audio = cut_silence(audio,sr=sr)
+    return audio
+
+
+# Returns the sample rate of the audio file
+def get_sr(filename: str, path: str):
+    audio, sr = librosa.load(path + filename)
+    return sr
+
+
+# Determines if we return the equivalent of "past" or "to" in English
+#def with_or_without(m: int):
+#    return 'to.wav' if m > 30 else 'and.wav'
+
+
+# Concatenate the audio files given an array of filename strings
+def concatenate_audio(filenames, lang_path):
+    audio = []
+    sr = get_sr(filenames[0], lang_path)
+    for name in filenames:
+        audio = np.concatenate((audio, read_audio(name, lang_path)))
+    return sr, audio
+
+
+# Convert array of file names to resulting concatenated audio, then plays that
+def play_audio(audio_names, speed_rate, lang_path):
+    sr, result_audio = concatenate_audio(audio_names, lang_path)
+    # Apply time stretching only if needed
+    if speed_rate != 1:
+        result_audio = librosa.effects.time_stretch(result_audio,
+                                                    rate=float(speed_rate))
+
+    # Convert to 16-bit integer format so the audio player understands it
+    #result_audio = np.array(result_audio, dtype=np.int16)
+    # Write the result as a .wav file
+    print(sr, result_audio)
+    sf.write('result.wav', result_audio, sr)
+    # Code related to playing the audio file
+    pygame.mixer.init()
+    aud = pygame.mixer.Sound('result.wav')
+    aud.play()
+    # Apply this so the audio can play without interruption
+    time.sleep(aud.get_length())
+    # Unload to make sure we can rewrite "result.wav" again in the same session
+    pygame.mixer.music.unload()
