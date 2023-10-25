19fffdc74a9c18a83a430049283820e255e01591
Yuanyuan Shi <144064507+erinshi1@users.noreply.github.com>
2023-10-26 01:01:40 +0200

    Fixed AM and PM

--- singapore.py
+++ singapore.py
@@ -1,12 +1,10 @@
 from settings import play_audio, get_current_time
 
-
 def get_which_meridium(hr: int):
     if hr > 12:
         return 'PM.wav'
     else:
         return 'AM.wav'
-
 
 def get_hour_filename(hr: int):
     if hr > 12:
@@ -14,24 +12,21 @@ def get_hour_filename(hr: int):
     path = 'hours/'
     return path + str(hr) + 'h.wav'
 
-
 def get_minute_filename(m: int):
     path = 'mins/'
     return path + str(m) + 'm.wav'
-
 
 def get_sec_filename(s: int):
     path = 'seconds/'
     return path + str(s) + 's.wav'
 
-
 def sg_clock(speed_rate=1, volume_level=1):
     hour, minute, second = get_current_time("Asia/Singapore")
 
-    if minute != 0:
-        audio_names = ['Hello.wav', 'Its.wav', get_hour_filename(hour), get_minute_filename(minute) ]
-
+    # Check if it's an even hour
+    if minute == 0:
+        audio_names = ['Hello.wav', 'Its.wav', get_hour_filename(hour), 'Oclock.wav', get_which_meridium(hour)]
     else:
-        audio_names = ['Hello.wav', 'Its.wav', get_hour_filename(hour), 'Oclock.wav']
+        audio_names = ['Hello.wav', 'Its.wav', get_hour_filename(hour), get_minute_filename(minute), get_which_meridium(hour)]
 
     play_audio(audio_names, speed_rate, volume_level, 'EnglishAudios/')
