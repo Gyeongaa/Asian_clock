dfe3498c7a34972035e1bc489edaf0bc71b52c9d
chenyi <c.lin.22@student.rug.nl>
2023-10-26 11:28:39 +0200

    the sloth voice is gone.

--- ChineseNatural.py
+++ ChineseNatural.py
@@ -1,19 +1,19 @@
-from settings import play_audio, get_current_time
-
-#These below function can be changed depends on the way you saved your file.
+from settings import get_current_time
+from pydub import AudioSegment
+from pydub.playback import play
+from pydub.utils import ratio_to_db
 
 def get_which_meridium(hr: int):
-    if hr> 12: #does the requirements need 12 hours formant?
+    if hr > 12:
         return 'PM.wav'
     else:
         return 'AM.wav'
 
 def get_hour_filename(hr: int):
     if hr > 12:
-        hr = hr%12
+        hr = hr % 12
     path = 'hours/'
-    return path+str(hr) + 'h.wav'
-
+    return path + str(hr) + 'h.wav'
 
 def get_minute_filename(m: int):
     path = 'mins/'
@@ -23,9 +23,34 @@ def ch_natural_clock(speed_rate=1, volume_level=1):
     hour, minute, second = get_current_time("Asia/Shanghai")
 
     if minute != 0:
-        audio_names = ['hello.wav','current_time.wav', get_which_meridium(hour), get_hour_filename(hour),
+        audio_names = ['hello.wav', 'current_time.wav', get_which_meridium(hour), get_hour_filename(hour),
                        get_minute_filename(minute)]
     else:
         audio_names = ['hello.wav', 'current_time.wav', get_which_meridium(hour), get_hour_filename(hour)]
 
-    play_audio(audio_names, speed_rate, volume_level, 'MandarinRecordings/')
+    # Generate file paths for all the audios involved
+    wav_file_paths = audio_names
+
+    # Define a list to store the audio segments
+    audio_segments = []
+
+    # Load and append the WAV files to the audio_segments list
+    for wav_file_path in wav_file_paths:
+        audio = AudioSegment.from_file(f"MandarinRecordings/{wav_file_path}")
+
+        # Adjust  speed rate
+        if speed_rate !=1:
+            audio = audio.speedup(playback_speed=speed_rate)
+
+        # Adjust volume by multiplying by volume_level (percentage) using dB conversion
+        db = ratio_to_db(volume_level)
+        audio = audio.apply_gain(db)
+
+        audio_segments.append(audio)
+
+    # Concatenate the audio segments
+    combined_audio = sum(audio_segments)
+
+    # Play the concatenated audio
+    play(combined_audio)
+
